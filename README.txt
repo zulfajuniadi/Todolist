289ba0194efdb8329b070a960444da537b23f2bc
Zulfa Juniadi <zulfajuniadi@gmail.com>
2013-11-03 01:19:18 +0800

    Initial Commit

---
+++ README.txt
@@ -0,0 +1,7 @@
+To use:
+
+1) Make sure you have meteor installed
+2) Create a new project: meteor create PROJECT_NAME
+3) Copy these files over into the new project folder you just created.
+4) Run meteor add accounts-ui
+5) Run meteor to start the server
---
+++ todolist.js
@@ -0,0 +1,73 @@
+TodoCollection = new Meteor.Collection("Todos");
+
+if (Meteor.isClient) {
+
+    Meteor.Collection.prototype.purge = function() {
+        var self = this;
+        this.find().fetch().forEach(function(record) {
+            self.remove(record._id);
+        });
+    }
+
+    Handlebars.registerHelper('formatDate', function(date){
+        return moment(date).format('D/M/YY H:mm:ss');
+    });
+
+    Template.todos.activeTodos = function(){
+        if(Meteor.userId()) {
+            return TodoCollection.find({$and : [{uid : Meteor.userId()}, {isDone : false}]}, {sort : {'insertedAt': -1}}).fetch();
+        }
+        return TodoCollection.find({isDone : false}, {sort : {'insertedAt': -1}});
+    };
+
+    Template.todos.doneTodos = function(){
+        if(Meteor.userId()) {
+            return TodoCollection.find({$and : [{uid : Meteor.userId()}, {isDone : true}]}, {sort : {'insertedAt': -1}}).fetch();
+        }
+        return TodoCollection.find({isDone : true}, {sort : {'insertedAt': -1}});
+    };
+
+    Template.todos.showDone = function() {
+        return Session.equals('showDone', true);
+    }
+
+    function save() {
+        var el = document.getElementById('newTodo');
+        var value = el.value;
+        var obj = {
+            name : el.value,
+            isDone : false,
+            insertedAt : new Date()
+        };
+
+        if(Meteor.userId()) {
+            obj.uid = Meteor.userId();
+        }
+        TodoCollection.insert(obj);
+        el.value = '';
+    }
+
+    Template.todos.events({
+        'click #save' : save,
+        'keyup #newTodo' : function(e){
+            if(e.keyCode === 13) {
+                save();
+            }
+        },
+        'change .done' : function(e) {
+            if(e.target.checked === true) {
+                return TodoCollection.update({_id : this._id}, {$set : {isDone : true, doneAt : new Date()}});
+            }
+            return TodoCollection.update({_id : this._id}, {$set : {isDone : false, doneAt : null}});
+        },
+        'change #showDone' : function(e) {
+            if(e.target.checked === true) {
+                return Session.set('showDone', true);
+            }
+            return Session.set('showDone', false);
+        },
+        'click .deleteTodo' : function(){
+            TodoCollection.remove({_id : this._id});
+        }
+    });
+}
